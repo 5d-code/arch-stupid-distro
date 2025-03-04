b9e571735d9dcd777ca3de32d585513d04db4473
5d-code <95885865+5d-code@users.noreply.github.com>
2025-03-04 17:54:12 +0200

    Add files via upload

--- builder.py
+++ builder.py
@@ -1,190 +1,158 @@
 import json
 import os
+from pathlib import Path
 
-executables = []
+# Constants
+ARCHISO_PATH = Path("./archiso")
+AIROOTFS_PATH = ARCHISO_PATH / "airootfs"
+CONFIG_PATH = Path("./distro/config.json")
+RES_PATH = Path("./res")
+SYS_LINUX_PATH = ARCHISO_PATH / "syslinux"
+EFI_BOOT_PATH = ARCHISO_PATH / "efiboot/loader/entries"
 
-def logruns(logmsg: str = None, endmsg: str = None):
+class Utility:
+    """Utility functions for file handling and execution."""
+    @staticmethod
+    def read_file(file_path: Path) -> str:
+        with open(file_path, 'r') as f:
+            return f.read()
+
+    @staticmethod
+    def read_json(file_path: Path) -> dict[str, object]:
+        with open(file_path, 'r') as f:
+            return json.load(f)
+
+    @staticmethod
+    def write_file(file_path: Path, content: str) -> None:
+        with open(file_path, 'w') as f:
+            f.write(content)
+
+class ExecutablesManager:
+    """Handles making files executable within the ISO."""
+    executables: list[str] = []
+
+    @staticmethod
+    def make_executable(file_path: Path) -> None:
+        os.chmod(file_path, 0o755)
+        ExecutablesManager.executables.append(str(file_path.relative_to(AIROOTFS_PATH)))
+        print(f' * Made executable {file_path}')
+
+def log(start_msg: str = "", end_msg: str = ""):
+    """Decorator to log function execution."""
     def decorator(func):
         def wrapper(*args, **kwargs):
-            if logmsg: print(logmsg)
-            x = func(*args, **kwargs)
-            if endmsg: print(endmsg, '\n')
-            return x
+            if start_msg:
+                print(start_msg)
+            result = func(*args, **kwargs)
+            if end_msg:
+                print(end_msg, '\n')
+            return result
         return wrapper
     return decorator
 
-def make_executable(fn: str):
-    os.chmod(fn, 0o755)
-    executables.append(fn.removesuffix('./archiso/airootfs'))
-    print(' * made executable', fn)
+class Config:
+    """Manages generating configuration files."""
+    @staticmethod
+    def generate_template(file_path: Path, data: dict[str, object]) -> str:
+        content = Utility.read_file(file_path)
+        for key, value in data.items():
+            content = content.replace(f'{{{key}}}', str(value))
+        return content
 
-def load_file(fn: str) -> str:
-    with open(fn, 'r') as f:
-        return f.read()
+    @staticmethod
+    def get_os_release(data: dict[str, object]) -> str:
+        return Config.generate_template(RES_PATH / "os-release", data)
 
-def load_json(fn: str) -> dict:
-    with open(fn, 'r') as f:
-        x = json.load(f)
-        return x
+    @staticmethod
+    def get_profile_def(data: dict[str, object]) -> str:
+        data["id-upper"] = data["id"].upper()
+        profile_content = Config.generate_template(RES_PATH / "profiledef.sh", data)
+        profile_content += '\n' + '\n'.join(f'  ["{exe}"]="0:0:755"' for exe in ExecutablesManager.executables)
+        return profile_content
 
-@logruns()
-def load_os_release(dat: dict = None) -> str:
-    if not dat: return load_file('./res/os-release')
-    f = load_os_release()
-    for k, v in dat.items():
-        f = f.replace('{' + k + '}', f'{v}')
-    return f
+class Builder:
+    """Handles system building tasks."""
+    def __init__(self, config: dict[str, object]):
+        self.config = config
 
-@logruns()
-def load_profiledef(dat: dict = None) -> str:
-    if not dat: return load_file('./res/profiledef.sh')
-    f = load_profiledef()
-    dat['id-upper'] = dat['id'].upper()
-    for k, v in dat.items():    
-        f = f.replace('{' + k + '}', f'{v}')
-    
-    lines = f.split('\n')[:-1]
-    for i in executables:
-        lines.append(f'  ["{i}"]="0:0:755"')
-    
-    f = '\n'.join(lines)
+    @log("Copying releng files")
+    def copy_releng(self):
+        os.system(f'cp -r /usr/share/archiso/configs/releng {ARCHISO_PATH}')
 
-    print()
-    print('[profiledef.sh]')
-    print(f)
-    print()
+    @log("Adding packages to live ISO")
+    def add_packages(self, packages: list[str]):
+        Utility.write_file(ARCHISO_PATH / "packages.x86_64", '\n'.join(packages))
 
-    return f
+    @log("Rebranding distro")
+    def rebrand_distro(self):
+        self.add_os_release()
+        self.replace_names(SYS_LINUX_PATH)
+        self.replace_names(EFI_BOOT_PATH)
 
-@logruns()
-def load_config() -> dict:
-    return load_json('./distro/config.json')
+    def replace_names(self, directory: Path):
+        for file in directory.iterdir():
+            if file.name != "splash.png":
+                self.replace_name(file)
 
-config = load_config()
-print('[config]')
-print(json.dumps(config, indent=4))
-print()
+    def replace_name(self, file_path: Path):
+        try:
+            content = Utility.read_file(file_path).replace("Arch Linux", self.config["name"])
+            Utility.write_file(file_path, content)
+            print(f' * Rebranded {file_path}')
+        except Exception as e:
+            print(f' - Failed to rebrand {file_path}: {e}')
 
-packages = [
-    'xorg-server',
-    'xorg-xinit',
-    'xorg-xrandr',
-    'xorg-xsetroot',
-    'xorg-xbacklight',
-    'xorg-xinput',
-    'xterm',
-    'i3',
-    'i3status',
-    'dmenu',
-    'lightdm',
-    'lightdm-gtk-greeter',
-    *config['packages']
-]
+    @log("Setting hostname")
+    def set_hostname(self):
+        Utility.write_file(AIROOTFS_PATH / "etc/hostname", self.config["liveiso_hostname"])
 
-@logruns('copying releng files')
-def copy_releng_files():
-    os.system('cp -r /usr/share/archiso/configs/releng ./archiso')
+    @log('Adding ./distro/home to root home')
+    def setup_distro_home(self):
+        os.makedirs(AIROOTFS_PATH / "root", exist_ok=True)
+        os.system(f'cp -r ./distro/home/* {AIROOTFS_PATH / "root"}')
+        
+        installer_path = (AIROOTFS_PATH / "root/installer")
+        if installer_path.exists():
+            ExecutablesManager.make_executable(installer_path)
 
-@logruns('adding packages to live iso')
-def add_packages():
-    for i in packages: print(f' * {i}')
-    with open('./archiso/packages.x86_64', 'a') as f:
-        f.write('\n'.join(packages))
-    print('packages added')
-    print()
+    @log('Adding ./res/home to root home')
+    def setup_res_home(self):
+        os.makedirs(AIROOTFS_PATH / "root/.config", exist_ok=True)
+        os.system(f'cp -r ./res/home/* {AIROOTFS_PATH / "root"}')
 
-@logruns('enabling autologin')
-def enable_autologin():
-    with open('./archiso/airootfs/etc/systemd/system/getty@tty1.service.d/autologin.conf', 'w') as f:
-        f.write(load_file('./res/autologin.conf'))
+        ExecutablesManager.make_executable(AIROOTFS_PATH / "root/.xinitrc")
+        ExecutablesManager.make_executable(AIROOTFS_PATH / "root/.zshrc")
+        ExecutablesManager.make_executable(AIROOTFS_PATH / "root/.config/i3/config")
 
-@logruns('adding xinitrc')
-def add_xinitrc():
-    p = './archiso/airootfs/root/.xinitrc'
-    with open(p, 'w') as f:
-        f.write(load_file('./res/xinitrc'))
-        make_executable(p)
+    @log("Setting up home directory")
+    def setup_home(self):
+        self.setup_distro_home()
+        self.setup_res_home()
 
-@logruns('adding zshrc')
-def add_zshrc():
-    p = './archiso/airootfs/root/.zshrc'
-    with open(p, 'w') as f:
-        f.write(load_file('./res/zshrc'))
-        make_executable(p)
+    @log("Adding OS release")
+    def add_os_release(self):
+        Utility.write_file(AIROOTFS_PATH / "etc/os-release", Config.get_os_release(self.config))
 
-@logruns('adding i3 config')
-def add_i3config():
-    p = './archiso/airootfs/root/.config/i3/config'
-    os.makedirs(os.path.dirname(p), exist_ok=True)
-    with open(p, 'w') as f:
-        f.write(load_file('./res/i3config'))
+    @log("Adding profile definition")
+    def add_profile_def(self):
+        Utility.write_file(AIROOTFS_PATH / "root/.profiledef", Config.get_profile_def(self.config))
 
-@logruns('adding splash')
-def add_splash():
-    os.system('cp ./distro/splash.png ./archiso/syslinux/splash.png')
+    @log("====\nBUILDING BASE\n====", "====\nBASE BUILT\n====")
+    def build(self, packages: list[str]):
+        self.copy_releng()
+        self.add_packages(packages)
+        self.rebrand_distro()
+        self.set_hostname()
+        self.setup_home()
+        self.add_profile_def()
+        print("\nISO is ready with i3 X11 environment, launching installer on boot.\n")
 
-def replace_distro_name(fn: str):
-    try:
-        with open(f'./archiso/syslinux/{fn}', 'r') as f:
-            dat = f.read()
-        with open(f'./archiso/syslinux/{fn}', 'w') as f:
-            f.write(dat.replace('Arch Linux', config['name']))
-        print(' * replaced Arch Linux with distro name on', fn)
-    except Exception as e:
-        print(' - failed to replace Arch Linux with distro name on', fn)
+if __name__ == "__main__":
+    config_data = Utility.read_json(CONFIG_PATH)
+    packages_list = [
+        "xorg-server", "xorg-xinit", "xorg-xrandr", "xorg-xsetroot", "xorg-xbacklight", "xorg-xinput", "xterm",
+        "i3", "i3status", "dmenu", "lightdm", "lightdm-gtk-greeter", *config_data['packages']
+    ]
 
-@logruns('rebranding Arch Linux', 'rebranded Arch Linux')
-def replace_all_distro_names(d: str = './archiso/syslinux'):
-    for i in os.listdir(d):
-        if i == 'splash.png': continue
-        replace_distro_name(i)
-
-@logruns('rebranding Arch Linux on boot entries')
-def replace_efi_boot_entries():
-    replace_all_distro_names('./archiso/efiboot/loader/entries')
-
-@logruns('setting iso hostname')
-def set_iso_hostname():
-    with open('./archiso/airootfs/etc/hostname', 'w') as f:
-        f.write(config['liveiso_hostname'])
-
-@logruns('adding home')
-def add_home():
-    os.makedirs('./archiso/airootfs/root', exist_ok=True)
-    os.system(f'cp -r ./distro/home/* ./archiso/airootfs/root')
-
-    make_executable('./archinstall/airootfs/root/installer')
-
-@logruns('add os-release')
-def add_os_release():
-    with open('./archiso/airootfs/etc/os-release', 'w') as f:
-        f.write(load_os_release(config))
-
-@logruns('add profiledef')
-def add_profiledef():
-    with open('./archiso/airootfs/root/.profiledef', 'w') as f:
-        f.write(load_profiledef(config))
-
-@logruns('====\nBUILDING BASE\n====', '====\nBASE BUILT\n====')
-def build_base():
-    print()
-
-    copy_releng_files()
-    add_packages()
-    replace_all_distro_names()
-    replace_efi_boot_entries()
-    set_iso_hostname()
-    enable_autologin()
-    add_xinitrc()
-    add_zshrc()
-    add_i3config()
-    add_splash()
-    add_home()
-    add_os_release()
-    add_profiledef()
-
-    print()
-    print('ISO now has an i3 X11 install enviroment, that will execute installer on boot.')
-    print()
-
-build_base()
+    builder = Builder(config_data)
+    builder.build(packages_list)
